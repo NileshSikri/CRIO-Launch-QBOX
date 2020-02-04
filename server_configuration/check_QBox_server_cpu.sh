80e0176f482ea3ae9f0484f6eeb985789d81ffff
NileshSikri <nilesh.kumarsikri@gmail.com>
2020-02-04 17:26:11 +0000

    ME_QBOX_MODULE_SERVER_CONFIGURATION

--- server_configuration/check_QBox_server_cpu.sh
+++ server_configuration/check_QBox_server_cpu.sh
@@ -21,9 +21,9 @@ requiredCPUSpeedInMHz=1000     # 1 GHz
     # 3) Bash script can only do integer arithmetic. Include additional handling needed to strip the decimal part
     # 4) Execute the script before submission and confirm the output 
 
-cpuSpeedInMHz=`lscpu | awk '{if(NR==15)print $3}'`
+cpuSpeedInMHz=` grep "cpu MHz" /proc/cpuinfo | awk 'NR==1{print $4}'`
 cpuSpeedInMHz=${cpuSpeedInMHz%.*}
- 
+
 if test $cpuSpeedInMHz -ge $requiredCPUSpeedInMHz
    then
    echo "CPU Speed of $cpuSpeedInMHz MHz is sufficient for QBox"
